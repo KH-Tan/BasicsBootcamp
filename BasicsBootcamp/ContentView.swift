fdc72e0f914a2e19c7a4b077d940d9d95d674ba5
KH-Tan <tankonghwi@gmail.com>
2024-05-01 11:55:13 +0800

    Changed ContentView

--- BasicsBootcamp/ContentView.swift
+++ BasicsBootcamp/ContentView.swift
@@ -14,6 +14,10 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Divider()
+            Text("Konnichi Wa")
+            Text("Hola")
  
         }
         .padding()
