098a1bc47de7ce4ab5106e5a9d246646bb2400e6
KH-Tan <tankonghwi@gmail.com>
2024-05-01 11:55:13 +0800

    Changed ContentView

---
+++ BasicsBootcamp/Advanced/UsingViewBuider.swift
@@ -0,0 +1,19 @@
+//
+//  UsingViewBuider.swift
+//  BasicsBootcamp
+//
+//  Created by KH Tan on 1/5/24.
+//
+
+import SwiftUI
+
+struct UsingViewBuider: View {
+    var body: some View {
+        Text("Hello, ViewBuilder!")
+        Text("Using View Builder")
+    }
+}
+
+#Preview {
+    UsingViewBuider()
+}
